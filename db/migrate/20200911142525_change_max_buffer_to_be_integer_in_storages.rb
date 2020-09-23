4402629a11678acae789d9ee0c428586c36f9d17
smohapatra200 <smohapatra200@gmail.com>
2020-09-23 14:59:26 +0530

    migration edit

--- db/migrate/20200911142525_change_max_buffer_to_be_integer_in_storages.rb
+++ db/migrate/20200911142525_change_max_buffer_to_be_integer_in_storages.rb
@@ -1,5 +1,5 @@
 class ChangeMaxBufferToBeIntegerInStorages < ActiveRecord::Migration[6.0]
   def change
-    change_column :storages, :max_buffer, :integer
+    change_column :storages, :max_buffer, :integer, using: "max_buffer::integer"
   end
 end
